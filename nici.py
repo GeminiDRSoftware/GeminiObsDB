5a7a0068b610df5f8b45b69efd9f6a101a5e153c
fitsdata <fitsdata>
2015-09-10 21:33:36 +0000

    enumify nici

--- nici.py
+++ nici.py
@@ -5,6 +5,15 @@ from sqlalchemy.orm import relation
 from . import Base
 from .header import Header
 
+FOCAL_PLANE_MASKS = ['Clear_G5710', 'F0.22_G5715', 'F0.32_G5714', 'F0.46_G5713', 
+                     'F0.65_G5712', 'F0.90_G5711', 'Grid_G5716']
+FOCAL_PLANE_MASK_ENUM = Enum(*FOCAL_PLANE_MASKS, name='nici_focal_plane_mask')
+
+DISPERSERS = ['Block', 'H-50/50_G5701', 'H-CH4-Dichroic_G5704', 
+              'H/K-Dichroic_G5705', 'Mirror_G5702', 'Open']
+DISPERSER_ENUM = Enum(*DISPERSERS, name='nici_disperser')
+
+
 class Nici(Base):
     """
     This is the ORM object for the NICI details
@@ -15,8 +24,8 @@ class Nici(Base):
     header_id = Column(Integer, ForeignKey('header.id'), nullable=False, index=True)
     header = relation(Header, order_by=id)
     filter_name = Column(Text, index=True)
-    focal_plane_mask = Column(Text, index=True)
-    disperser = Column(Text, index=True)
+    focal_plane_mask = Column(FOCAL_PLANE_MASK_ENUM, index=True)
+    disperser = Column(DISPERSER_ENUM, index=True)
 
     def __init__(self, header, ad):
         self.header = header
@@ -26,5 +35,12 @@ class Nici(Base):
 
     def populate(self, ad):
         self.filter_name = ad.filter_name().for_db()
-        self.focal_plane_mask = ad.focal_plane_mask().for_db()
-        self.disperser = ad.disperser().for_db()
+      
+        focal_plane_mask = ad.focal_plane_mask().for_db()
+        if focal_plane_mask in FOCAL_PLANE_MASKS:
+            self.focal_plane_mask = focal_plane_mask
+
+        disperser = ad.disperser().for_db()
+        if disperser in DISPERSERS:
+            self.disperser = disperser
+
