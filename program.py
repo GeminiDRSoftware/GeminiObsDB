744e53af4535edeb17a65ac2efa7c93e9dbf6268
Paul Hirst <phirst@gemini.edu>
2016-11-09 21:07:56 +0000

    initial cut at program ORM

---
+++ program.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer, Text, Boolean, Float
+
+from . import Base
+
+class Program(Base):
+    """
+    This is the ORM class for storing program details fetched from the ODB.
+    """
+    __tablename__ = 'program'
+
+    id = Column(Integer, primary_key=True)
+    program_id = Column(Integer, index=True)
+    pi_coi_names = Column(Text, index=True)
+    title = Column(Text)
+    abstract = Column(Text)
+    piemail = Column(Text)
+    coiemail = Column(Text)
+    science_band = Column(Integer)
+    partners = Column(Text)
+    rollover = Column(Boolean)
+    too = Column(Boolean)
+    completion = Column(Text)
+    allocated_hours = Column(Float)
+    remaining_hours = Column(Float)
+    last_refreshed = Column(DateTime, index=True)
+
+    def __init__(self, program_id):
+        self.program_id = program_id
+
