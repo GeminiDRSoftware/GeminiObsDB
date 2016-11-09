f762640969865312eda24cf79e45631150444e1a
Paul Hirst <phirst@gemini.edu>
2016-11-09 21:08:57 +0000

    initial cut at obslog comment ORM

---
+++ obslog_comment.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer, Text, Boolean
+
+from . import Base
+
+class ObslogComment(Base):
+    """
+    This is the ORM class for storing observation log comments retrieved from the ODB. Note, these are not related (in the FitsStorage world) to the obslog files we also store.
+    """
+    __tablename__ = 'obslog_comment'
+
+    id = Column(Integer, primary_key=True)
+    program_id = Column(Text, index=True)
+    data_label = Column(Text, index=True)
+    comment = Column(Text)
+
+    def __init__(self, program_id, data_label, comment):
+        self.program_id = program_id
+        self.data_label = data_label
+        self.comment = comment
+
