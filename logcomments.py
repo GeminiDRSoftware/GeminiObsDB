ef518f79f2a2044ce4f268a07961f297a1a6fdf3
Ricardo Cardenes <rcardenes@gemini.edu>
2016-11-09 18:21:22 +0000

    Added skeleton for ingesting programs/obslog comments

---
+++ logcomments.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy import Integer, Text
+from .header import Header
+
+class LogComments(Base):
+    """
+    This is the ORM class for the table containing obslog comments for
+    images (extracted from ODB).
+    """
+    __tablename__ = 'logcomments'
+
+    id = Column(Integer, primary_key=True)
+    header_id = Column(Integer, ForeignKey('header.id'), nullable=False, index=True)
+    comment = Text()
+
+    def __init__(self, header, comment=None):
+        self.header_id = header.id
+        if comment is not None:
+            self.comment = comment
+
