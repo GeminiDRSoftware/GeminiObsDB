5a7266a67dad8590f292c1eb26bd6fecbc383a4f
Kenneth Anderson <kanderso@gemini.edu>
2018-04-26 16:21:22 +0000

    update on Trac #279, handle weird obslog names

--- obslog.py
+++ obslog.py
@@ -1,17 +1,40 @@
-from sqlalchemy import Column, ForeignKey
-from sqlalchemy import Integer, Text, Date
-from sqlalchemy.orm import relation
+"""
+The Oblog class attempts to determine the observation date and program id from 
+the obslog filename. Nominally, such a filename looks like,
 
+    <yyyymmdd>_<progid>_obslog.txt>.
+
+However, certain obslogs (only Phoenix instrument obslogs as far as is known)
+have names like, 
+
+    <yyyymmmdd>_obslog.txt,
+
+where mmm is string of the form 'Apr' or 'May', etc. .
+
+A re.match() is attempted on the first regex pattern, which matches the first
+filename kind. If that match returns None, a second regex pattern match is 
+attempted with a regex pattern matching the second possible form of the filename.
+The MONMAP provides the mapping from month name to month number, which must be
+used for building datetime.datetime objects.
+
+"""
 import re
 import datetime
 import dateutil.parser
 
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy import Integer, Text, Date
+from sqlalchemy.orm import relation
+
 from . import Base
 from .diskfile import DiskFile
-
-OBSLOG_CRE = re.compile(r'^(20\d\d\d\d\d\d)_(.*)_obslog.txt')
-
 # ------------------------------------------------------------------------------
+MONMAP = {'jan': 1, 'feb': 2, 'mar': 3, 'apr': 4, 'may': 5, 'jun': 6,
+          'jul': 7, 'aug': 8, 'sep': 9, 'oct': 10, 'nov': 11, 'dec': 12
+}
+OBSLOG_CRE = re.compile(r'^(20\d\d\d\d\d\d)_(.*)_obslog.txt')
+OBSLOG_RE2 = re.compile(r'(^\d\d\d\d)([a-zA-Z][a-zA-Z][a-zA-Z])(\d\d)_obslog.txt')
+# ------------------------------------------------------------------------------ 
 class Obslog(Base):
     """
     This is the ORM class for the obslog table
@@ -33,6 +56,24 @@ class Obslog(Base):
                 datestr = match.group(1)
                 self.date = dateutil.parser.parse(datestr).date()
                 self.program_id = match.group(2)
+            else:
+                match = OBSLOG_RE2.match(filename)
+                if match:
+                    self.date = self._secondary_match(match)
+                    self.program_id = None
 
     def __repr__(self):
-        return "<Obslog('%s', '%s', '%s')>" % (self.id, self.program_id, self.date)
+        report = "<Obslog('{}', '{}', '{}')>"
+        return report.format(self.id, self.program_id, self.date)
+
+    def _secondary_match(self, match):
+        year = int(match.group(1))
+        day = int(match.group(3))
+        try:
+            mon = MONMAP[match.group(2).lower()]
+        except KeyError:
+            # if we still haven't scrapped a date from the filename,
+            # fix it to an quasi-arbitrary date; 1990-01-01.
+            return datetime.datetime(1990, 1, 1).date()
+
+        return datetime.datetime(year, mon, day)
