078ed448e22129e1e55599e97e3a0e704f9cbeff
Oliver <oly@oberdorf.org>
2020-01-17 13:25:12 -1000

    added grating order to gmos and to arc cal query

--- gmos.py
+++ gmos.py
@@ -40,6 +40,7 @@ class Gmos(Base):
     prepared = Column(Boolean, index=True)
     overscan_subtracted = Column(Boolean, index=True)
     overscan_trimmed = Column(Boolean, index=True)
+    grating_order = Column(Integer)
 
     def __init__(self, header, ad):
         self.header = header
@@ -86,6 +87,12 @@ class Gmos(Base):
             except:
                 self.nod_count = None
             self.nod_pixels = ad.shuffle_pixels()
+        
+        try:
+            grating_order = ad.header['GRORDER']
+            self.grating_order = grating_order
+        except:
+            self.grating_order = None
         self.prepared = 'PREPARED' in ad.tags
         self.overscan_trimmed = 'OVERSCAN_TRIMMED' in ad.tags
         self.overscan_subtracted = 'OVERSCAN_SUBTRACTED' in ad.tags
