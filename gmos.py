7a82107dda91f1d3a101a57cd915ce5eb040b28f
Oliver <oly@oberdorf.org>
2020-01-24 09:09:42 -1000

    Removing grating order until we have a clean new implementation in astrodata

--- gmos.py
+++ gmos.py
@@ -40,7 +40,7 @@ class Gmos(Base):
     prepared = Column(Boolean, index=True)
     overscan_subtracted = Column(Boolean, index=True)
     overscan_trimmed = Column(Boolean, index=True)
-    grating_order = Column(Integer)
+    # grating_order = Column(Integer)
 
     def __init__(self, header, ad):
         self.header = header
@@ -88,11 +88,11 @@ class Gmos(Base):
                 self.nod_count = None
             self.nod_pixels = ad.shuffle_pixels()
         
-        try:
-            grating_order = ad.phu.get('GRORDER')
-            self.grating_order = grating_order
-        except:
-            self.grating_order = None
+        # try:
+        #     grating_order = ad.phu.get('GRORDER')
+        #     self.grating_order = grating_order
+        # except:
+        #     self.grating_order = None
         self.prepared = 'PREPARED' in ad.tags
         self.overscan_trimmed = 'OVERSCAN_TRIMMED' in ad.tags
         self.overscan_subtracted = 'OVERSCAN_SUBTRACTED' in ad.tags
