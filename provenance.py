7008149b15a629ce3dcaf6e9516196b585475f4b
Oliver <oly@oberdorf.org>
2020-11-02 17:23:10 -1000

    reverting diskfile import, not needed for migrationa fter all

--- provenance.py
+++ provenance.py
@@ -3,7 +3,6 @@ from datetime import datetime
 
 from sqlalchemy import Column, ForeignKey
 from sqlalchemy import Integer, Text, DateTime, String
-from .diskfile import DiskFile
 
 from . import Base
 
