5786177b6aadc2f8b0d11fcc0db042dcfed7313e
Oliver <oly@oberdorf.org>
2021-07-13 11:06:14 -1000

    removing circular dependency on FitsStorage

--- gemini_obs_db/db/db_config.py
+++ gemini_obs_db/db/db_config.py
@@ -29,15 +29,15 @@ postgres_database_max_overflow = 10
 # Slightly naughty cyclical dependency on FitsStorage remains here.  This trick will pull in
 # the FitsStorage configuration, if it exists.  If we are not running within the FitsStore, the
 # import will fail which is fine.  In that case, we just want the settings as above.
-try:
-    from fits_storage import fits_storage_config
-    using_apache = True
-    use_utc = fits_storage_config.use_as_archive
-    storage_root = fits_storage_config.storage_root
-    z_staging_area = fits_storage_config.z_staging_area
-    database_url = fits_storage_config.fits_database
-    postgres_database_pool_size = fits_storage_config.fits_database_pool_size
-    postgres_database_max_overflow = fits_storage_config.fits_database_max_overflow
-except:
-    # ok, not running in FITS Server context
-    pass
+# try:
+#     from fits_storage import fits_storage_config
+#     using_apache = True
+#     use_utc = fits_storage_config.use_as_archive
+#     storage_root = fits_storage_config.storage_root
+#     z_staging_area = fits_storage_config.z_staging_area
+#     database_url = fits_storage_config.fits_database
+#     postgres_database_pool_size = fits_storage_config.fits_database_pool_size
+#     postgres_database_max_overflow = fits_storage_config.fits_database_max_overflow
+# except:
+#     # ok, not running in FITS Server context
+#     pass
